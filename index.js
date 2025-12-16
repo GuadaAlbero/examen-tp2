2f0066dbe14e8f64f40c45f233f07761df2b5557
Guada <guadalupealbero@gmail.com>
2025-12-16 16:27:12 -0300

    Ajustes para el examen

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import router from "./router/router.js";
-import errorHandler from "./middlewares/errorHandler.js";
 
 
 const app = express();
@@ -9,7 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
-app.use(errorHandler);
 
 app.listen(8080, () => {
   console.log("server ok in 8080");
